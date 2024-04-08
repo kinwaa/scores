172579d699d4af3b69694f39503f32121d3eb9a0
Kinwaa <andy19feb@gmail.com>
2024-04-08 21:41:50 +0800

    上传处理代码

---
+++ score-toast/src/main/java/cc/waa/tools/scoretoast/ApplicationStarter.java
@@ -0,0 +1,10 @@
+package cc.waa.tools.scoretoast;
+
+import static org.springframework.boot.SpringApplication.run;
+
+public class ApplicationStarter {
+
+   public static void main(String[] args) {
+      run(ScoreToastApplication.class, args);
+   }
+}
---
+++ score-toast/src/main/java/cc/waa/tools/scoretoast/ScoreToastApplication.java
@@ -0,0 +1,97 @@
+package cc.waa.tools.scoretoast;
+
+import static java.util.Arrays.stream;
+import static javax.xml.xpath.XPathConstants.NODESET;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class ScoreToastApplication implements CommandLineRunner {
+
+   private void cleanNodes(XPath xpath, Document doc, String path) throws Exception {
+      NodeList list = (NodeList) xpath.evaluate(path, doc, NODESET);
+
+      for (int i = 0; i < list.getLength(); i++) {
+         Node node = list.item(i);
+         node.getParentNode().removeChild(node);
+      }
+   }
+
+   private void cleanAttributes(XPath xpath, Document doc, String path, String... attrNames) throws Exception {
+      if (attrNames == null || attrNames.length <= 0) {
+         return;
+      }
+
+      NodeList list = (NodeList) xpath.evaluate(path, doc, NODESET);
+
+      for (int i = 0; i < list.getLength(); i++) {
+         Node node = list.item(i);
+         NamedNodeMap attrs = node.getAttributes();
+
+         stream(attrNames)
+               .filter(d -> attrs.getNamedItem(d) != null)
+               .forEach(d -> attrs.removeNamedItem(d));
+      }
+   }
+
+   @Override
+   public void run(String... args) throws Exception {
+      final File file;
+
+      if (args == null || args.length <= 0) {
+         log.error("no arguments");
+
+         return;
+      }
+
+      if ((file = new File(args[0])) == null || !file.exists()) {
+         log.error("file not exists");
+
+         return;
+      }
+
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder builder = factory.newDocumentBuilder();
+
+      Document doc = builder.parse(file);
+
+      XPathFactory pathFactory = XPathFactory.newInstance();
+      XPath xpath = pathFactory.newXPath();
+
+      cleanNodes(xpath, doc, "//score-partwise/identification/encoding");
+      cleanNodes(xpath, doc, "//score-partwise/identification/source");
+      cleanNodes(xpath, doc, "//score-partwise/defaults");
+      cleanNodes(xpath, doc, "//score-partwise/credit");
+      cleanNodes(xpath, doc, "//score-partwise/part/measure/print");
+      cleanNodes(xpath, doc, "//score-partwise/part/measure/note/beam");
+      cleanNodes(xpath, doc, "//score-partwise/part/measure/note/stem");
+      cleanAttributes(xpath, doc, "//score-partwise/part/measure", "width");
+      cleanAttributes(xpath, doc, "//score-partwise/part/measure/note", "default-x", "default-y");
+      cleanAttributes(xpath, doc, "//score-partwise/part/measure/note/lyric", "default-x", "default-y", "relative-y");
+
+      TransformerFactory txFactory = TransformerFactory.newInstance();
+      Transformer transformer = txFactory.newTransformer();
+      transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+      transformer.transform(new DOMSource(doc), new StreamResult(file));
+   }
+}
